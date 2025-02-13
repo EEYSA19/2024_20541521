315ae38e00013356705133b803ada9b342cec372
Samuel Alabi <eeysa19@nottingham.ac.uk>
2025-02-13 15:38:38 +0000

    adder

--- Worksheet5/adder.cpp
+++ Worksheet5/adder.cpp
@@ -1,5 +1,22 @@
 #include "adder.h"
 
+/**
+ * @file adder.cpp
+ * @brief Implements the addition function.
+ *
+ * This file contains the implementation of the `add` function, 
+ * which performs integer addition.
+ */
+
+/**
+ * @brief Adds two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a First integer.
+ * @param b Second integer.
+ * @return The sum of `a` and `b`.
+ */
 int add(int a, int b) {
     return a + b;
 }
