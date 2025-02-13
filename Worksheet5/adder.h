5c24038338803140bc123fdc8ee209636fd9420a
Samuel Alabi <eeysa19@nottingham.ac.uk>
2025-02-13 15:02:31 +0000

    adder.h file

--- Worksheet5/adder.h
+++ Worksheet5/adder.h
@@ -1,6 +1,18 @@
 #ifndef MATHSLIB_ADDER_H
 #define MATHSLIB_ADDER_H
+/** @file
+ *  @brief This file contains the declarations of all exported functions in the maths library.
+ */
 
+/**
+ * @brief Adding function
+ * 
+ * This function adds two numbers.
+ *
+ * @param a The first number
+ * @param b The second number
+ * @return The sum of a and b
+ */
 int add(int a, int b);
 
 #endif
